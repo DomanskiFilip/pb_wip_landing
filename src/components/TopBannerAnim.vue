a28bbfe679588ea856b3f7d9fc03c67310eccc9f
DomanskiFilip <filip_domanski@outlook.com>
2026-01-14 19:12:28 +0000

    banner title img/logo added

--- src/components/TopBannerAnim.vue
+++ src/components/TopBannerAnim.vue
@@ -1,11 +1,17 @@
 <template>
   <div class="video-wrapper">
     <video autoplay muted loop playsinline id="top-banner-video">
-      <source src="../assets/videos/MCCM-Banner2-v9.mp4" type="video/mp4" />
+      <source :src="banner" type="video/mp4" />
       Your browser does not support the video tag.
     </video>
+    <img :src="banner_title" alt="Banner Title Image" />
   </div>
 </template>
+
+<script setup lang="ts">
+import banner from '../assets/videos/MCCM-Banner2-v9.mp4';
+import banner_title from '../assets/images/banner-title.png';
+</script>
 
 <style scoped>
 .video-wrapper {
@@ -18,9 +24,23 @@
 }
 
 #top-banner-video {
+  position: absolute;
+  top: 0;
+  left: 0;
   width: 100%;
   height: 100%;
   object-fit: cover;
   object-position: center;
+  z-index: 0;
+}
+
+img {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 1;
+  width: 50%;
+  height: 35%;
 }
 </style>
