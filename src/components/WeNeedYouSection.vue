1ce8923465eae3f6e90fa522267b50ababeffce2
DomanskiFilip <filip_domanski@outlook.com>
2026-06-01 15:57:01 +0200

    path fix

--- src/components/WeNeedYouSection.vue
+++ src/components/WeNeedYouSection.vue
@@ -14,7 +14,7 @@
 
 <script setup lang="ts">
 import { computed } from "vue";
-import YouImage from "~/assets/images/YOU.png";
+import YouImage from "../assets/images/YOU.png";
 
 const props = defineProps<{
   stepIndex: number;
